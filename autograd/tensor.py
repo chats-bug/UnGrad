67e41597c01109227220f7bb217ba4dfe7280328
chats-bug <71580082+chats-bug@users.noreply.github.com>
2023-06-23 03:51:11 +0530

    added matmul op, op symbols support

--- autograd/tensor.py
+++ autograd/tensor.py
@@ -1,14 +1,21 @@
-from typing import NamedTuple, Callable, Optional
+from typing import NamedTuple, Callable, Optional, Union
 import numpy as np
 
 
-Arrayable = float | list | np.ndarray
-
+Arrayable = Union[float, list, np.ndarray]
 
 def ensure_array(arrayable: Arrayable) -> np.ndarray:
 	if isinstance(arrayable, np.ndarray):
 		return arrayable
 	return np.array(arrayable)
+
+
+Tensorable = Union['Tensor', float, np.ndarray]
+
+def ensure_tensor(tensorable: Tensorable) -> 'Tensor':
+	if isinstance(tensorable, Tensor):
+		return tensorable
+	return Tensor(tensorable)
 
 
 class Dependency(NamedTuple):
@@ -23,25 +30,80 @@ class Tensor:
 			requires_grad: bool = False,
 			depends_on: list[Dependency] = [],
 	) -> None:
-		self.data = ensure_array(data)
+		self._data = ensure_array(data)
 		self.requires_grad = requires_grad
 		self.depends_on = depends_on
-		self.shape = self.data.shape
-		self.grad: Tensor | None = None
+		self.shape = self._data.shape
+		self.grad: Optional[Tensor] = None
 
 		if self.requires_grad:
 			self.zero_grad()
 
+	@property
+	def data(self) -> np.ndarray:
+		return self._data
+	
+	@data.setter
+	def data(self, new_data: Arrayable) -> None:
+		self._data = ensure_array(new_data)
+		# If we change the data, we need to invalidate the gradient
+		self.grad = None
+
 	def __repr__(self) -> str:
-		return f"Tensor({self.data}), requires_grad=({self.requires_grad})"
+		return f"Tensor({self._data}), requires_grad=({self.requires_grad})"
+	
+	def __add__(self, other: 'Tensor') -> 'Tensor':
+		return add(self, ensure_tensor(other))
+	
+	def __radd__(self, other: 'Tensor') -> 'Tensor':
+		return add(self, ensure_tensor(other))
+	
+	def __iadd__(self, other: 'Tensor') -> 'Tensor':
+		"""
+		In-place add
+		t += other
+		"""
+		self.data += ensure_tensor(other).data
+		# In-place operations don't create new tensors
+		# so we don't need to create a new tensor here
+		return self
+	
+	def __mul__(self, other: 'Tensor') -> 'Tensor':
+		return mul(self, ensure_tensor(other))
+	
+	def __rmul__(self, other: 'Tensor') -> 'Tensor':
+		return mul(self, ensure_tensor(other))
+	
+	def __imul__(self, other: 'Tensor') -> 'Tensor':
+		self.data *= ensure_tensor(other).data
+		return self
+	
+	def __neg__(self) -> 'Tensor':
+		return neg(self)
+	
+	def __sub__(self, other: 'Tensor') -> 'Tensor':
+		return sub(self, ensure_tensor(other))
+	
+	def __rsub__(self, other: 'Tensor') -> 'Tensor':
+		return sub(self, ensure_tensor(other))
+	
+	def __isub__(self, other: 'Tensor') -> 'Tensor':
+		self.data = self.data - ensure_tensor(other).data
+		return self
+	
+	def __matmul__(self, other: 'Tensor') -> 'Tensor':
+		return matmul(self, other)
+	
+	def __getitem__(self, idxs) -> 'Tensor':
+		return slice(self, idxs)
 	
 	def zero_grad(self) -> None:
-		self.grad = Tensor(np.zeros_like(self.data))
+		self.grad = Tensor(np.zeros_like(self.data, dtype=np.float64))
 	
 	def sum(self) -> 'Tensor':
 		return tensor_sum(self)
 	
-	def backward(self, grad: 'Tensor' = None) -> None:
+	def backward(self, grad: Optional['Tensor'] = None) -> None:
 		assert self.requires_grad, "called backward on non-requires-grad tensor"
 
 		if grad is None:
@@ -174,3 +236,48 @@ def neg(t: Tensor) -> Tensor:
 
 def sub(t1: Tensor, t2: Tensor) -> Tensor:
 	return add(t1, neg(t2))
+
+
+def matmul(t1: Tensor, t2: Tensor) -> Tensor:
+	"""
+	If t3 = t1 @ t2, then the grads are as follows:
+		grad_1 = grad_3 @ t2.T
+		grad_2 = t1.T @ grad_3
+	"""
+	data = t1.data @ t2.data
+	requires_grad = t1.requires_grad or t2.requires_grad
+	depends_on: list[Dependency] = []
+
+	if t1.requires_grad:
+		def grad_fn1(grad: np.ndarray) -> np.ndarray:
+			return grad @ t2.data.T
+		
+		depends_on.append(Dependency(t1, grad_fn1))
+
+	if t2.requires_grad:
+		def grad_fn2(grad: np.ndarray) -> np.ndarray:
+			return t1.data.T @ grad
+		
+		depends_on.append(Dependency(t2, grad_fn2))
+
+	return Tensor(data, requires_grad, depends_on)
+
+
+def slice(t: Tensor, idx: slice) -> Tensor:
+	data = t.data[idx]
+	requires_grad = t.requires_grad
+	depends_on: list[Dependency] = []
+
+	if requires_grad:
+		def grad_fn(grad: np.ndarray) -> np.ndarray:
+			"""
+			grad is necessarily a 0-tensor, so each element
+			contributes that much
+			"""
+			b_grad = np.zeros_like(data)
+			b_grad[idx] = grad
+			return b_grad
+		
+		depends_on = [Dependency(t, grad_fn)]
+	
+	return Tensor(data, requires_grad, depends_on)
