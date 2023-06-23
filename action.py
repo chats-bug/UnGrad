4e8fa2e0a76558db26cc82d0dc47880629927d1c
chats-bug <71580082+chats-bug@users.noreply.github.com>
2023-06-23 13:54:25 +0530

    new way to grad. descent

--- action.py
+++ action.py
@@ -2,7 +2,7 @@
 Use the `Tensor` library to do cool things
 """
 import numpy as np
-from autograd.tensor import Tensor
+from autograd import Tensor, Parameter, Module, SGD
 
 
 def minimize_function():
@@ -29,8 +29,16 @@ def learn_simple_function():
 	coef = Tensor(np.random.randn(d))
 	y = X @ coef + np.random.randint(0, 10) + np.random.randn(n)/100
 
-	w = Tensor(np.random.randn(X.shape[1]), requires_grad=True)
-	b = Tensor(np.random.randn(), requires_grad=True)
+	class Model(Module):
+		def __init__(self, *shape) -> None:
+			self.w = Parameter(*shape)
+			self.b = Parameter()
+
+		def predict(self, inputs: Tensor):
+			return inputs @ self.w + self.b
+
+	
+	model = Model(d)
 
 	num_epochs = 100
 	batch_size = 32
@@ -42,23 +50,22 @@ def learn_simple_function():
 	print(f"Number of epochs: {num_epochs}")
 	print(f"Number of gradient accumulation steps: {ga_steps}")
 
+	optimizer = SGD(lr)
 	for epoch in range(num_epochs):
-		w.zero_grad()
-		b.zero_grad()
+		model.zero_grad()
 		
 		for steps in range(num_steps):
 			inputs = X[steps*batch_size:(steps+1)*batch_size]
-			y_preds = inputs @ w + b
+			y_preds = model.predict(inputs)
 			err = y_preds - y[steps*batch_size:(steps+1)*batch_size]
 			loss = (err * err).sum()
 
 			loss.backward()
 			
 			if (steps + 1) % ga_steps == 0 or steps == num_steps - 1:
-				w -= w.grad * lr
-				w.zero_grad()
-				b -= b.grad * lr
-				b.zero_grad()
+				optimizer.step(model)
+				model.zero_grad()
+
 				print(f"Epoch {epoch}: loss = {loss.data:.3f} -> Grad Accumulation Step {steps + 1}")
 			else:
 				print(f"Epoch {epoch}: loss = {loss.data:.3f}")
@@ -69,7 +76,7 @@ def learn_simple_function():
 	print(f"Number of gradient accumulation steps: {ga_steps}")
 
 	# Final loss
-	y_preds = X @ w + b
+	y_preds = model.predict(X)
 	err = y_preds - y
 	loss = (err * err).sum()
 	print(f"Final loss: {loss.data:.3f}")
---
+++ autograd/__init__.py
@@ -0,0 +1,4 @@
+from autograd.parameter import Parameter
+from autograd.tensor import Tensor
+from autograd.module import Module
+from autograd.optimizer import SGD, Adam
